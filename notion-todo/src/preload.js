c0228056946a771f697ca3953aa77c0693eb0301
lixiang19 <1506674357@qq.com>
2021-08-09 00:45:30 +0800

    接入notion

--- notion-todo/src/preload.js
+++ notion-todo/src/preload.js
@@ -1,5 +1,10 @@
 const path = require('path')
 window.path = path
+const getDirPath = (filePath) => path.join(__dirname, filePath)
+
+const loadModule = require(getDirPath('../../helper/loadModule.js'))
+
+loadModule.init()
 
 window.exports = {
   notion: { // 注意：键对应的是 plugin.json 中的 features.code
@@ -7,39 +12,17 @@ window.exports = {
     args: {
       // 进入插件时调用（可选）
       enter: (action, callbackSetList) => {
-        const getDirPath = (filePath) => path.join(__dirname, filePath)
-
-        const loadModule = require(getDirPath('../../helper/loadModule.js'))
-
-        loadModule.init()
       },
       // 子输入框内容变化时被调用 可选 (未设置则无搜索)
       search: (action, searchWord, callbackSetList) => {
-        // 获取一些数据
 
-        // 执行 callbackSetList 显示出来
-        // utools.ubrowser.goto('https://cn.bing.com')
-        //   .value('#sb_form_q', 'uTools')
-        //   .click('#sb_form_go')
-        //   .run({ width: 1000, height: 600 })
-        callbackSetList([
-          {
-            title: '输入框',
-            description: '这是描述',
-            icon: '', // 图标
-            url: 'https://yuanliao.info'
-          }
-        ])
       },
       // 用户选择列表中某个条目时被调用
       select: (action, itemData, callbackSetList) => {
-        window.utools.hideMainWindow()
-        const url = itemData.url
-        require('electron').shell.openExternal(url)
-        window.utools.outPlugin()
+
       },
       // 子输入框为空时的占位符，默认为字符串"搜索"
-      placeholder: '搜索'
+      placeholder: '添加燃烧日程'
     }
   }
 }
